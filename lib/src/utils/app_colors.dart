040ea6eb8eb3691adf36545267501e5a89fe0d87
LucasBatistaX <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>
2026-03-12 15:19:52 -0300

    Organizando as pastas

---
+++ lib/src/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Standard colors.
+  static const Color  title = Color(0xff0D0D0D);
+  static const Color subtitle = Color(0xff737373);
+
+}
